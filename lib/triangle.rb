21744b188eb5ba9d17114a1a203a6a09abc2fa7e
JoyTriza <wangarijoytriza@gmail.com>
2022-06-15 15:33:30 +0300

    commit tree 3b568ac2ad75ff200dd5185b216410dd1b9060ce

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,35 @@
 class Triangle
-  # write code here
+
+  attr_reader :x, :y, :z
+  def initialize(x, y, z)
+    @x = x
+    @y = y
+    @z = z
+  end
+
+  def kind
+    validate_triangle
+    if x == y && y == z
+      :equilateral
+    elsif x == y || y == z || x == z
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  def is_larger_than_zero?
+    [x, y, z].all?(&:positive?)
+  end
+
+  def is_larger_than_two?
+    x + y > z && x + z > y && y + z > x
+  end
+
+  def validate_triangle
+    raise TriangleError unless is_larger_than_zero? && is_larger_than_two?
+  end
+
+  class TriangleError < StandardError
+  end
 end
